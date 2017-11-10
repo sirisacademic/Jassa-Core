8a127b62052e7cc1af0be661255d1c84593e2232
Xavi Gimenez <xavi.gimenez@sirisacademic.com>
2017-11-10 10:53:35 +0100

    AggregatorSpecification now receiving vars instead of var names

--- dist/jassa.js
+++ dist/jassa.js
@@ -18053,16 +18053,29 @@ var Class = require('../../ext/Class');
 // constructor
 var AggregatorSpecification = Class.create({
     classLabel: 'jassa.sparql.AggregatorSpecification',
-    initialize: function(varName, isDistinct, aggregationType, aggregationNameAs) {
-        this.varName = varName;
+    initialize: function(v, isDistinct, aggregationType, aggVarAs) {
+        this.v = v;
         this.isDistinct = isDistinct;
         this.aggregationType = aggregationType;
-        this.aggregationNameAs = aggregationNameAs;
+        
+        // if var aggVarAs is ommited, create it based on 
+        // the source var and the aggregation function
+        this.aggVarAs = aggVarAs || rdf.NodeFactory.createVar(aggregationType + '_' + v.getName());
     },
 
+    getVarsMentioned: function() {
+        var result = [];
+        NodeUtils.pushVar(result, this.v);
+        NodeUtils.pushVar(result, this.aggVarAs);
+        return result;
+    },
 
-    getAggregationNameAs : function() {
-        return this.aggregationNameAs?   this.aggregationNameAs : (this.aggregationType + '_' + this.varName);
+    getVar : function() {
+        return this.v;
+    },
+
+    getAggVar : function() {
+        return this.aggVarAs;
     }
 });
 
